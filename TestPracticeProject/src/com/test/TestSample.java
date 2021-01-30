77c0131fa069606ccc99dd5de2ff667c3d37c662
NandhineeR <nandhinee2019@gmail.com>
2021-01-30 13:10:46 +0530

    one text removed

--- TestPracticeProject/src/com/test/TestSample.java
+++ TestPracticeProject/src/com/test/TestSample.java
@@ -4,6 +4,6 @@ public class TestSample {
 public static void main(String[] args) {
 	System.out.println("Lets Start");
 	System.out.println("Hi Team !!");
-	System.out.println("haiii");
+	System.out.println("hai");
 }
 }
